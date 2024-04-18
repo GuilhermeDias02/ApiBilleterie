67e68430af8623633c4757d53fe6c3a9fb4ddd72
GuilhermeDias02 <guilherme.alex.dias@gmai.com>
2024-04-18 09:51:17 +0200

    init

---
+++ src/test/java/fr/efrei/billeterie/BilleterieApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.billeterie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleterieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
